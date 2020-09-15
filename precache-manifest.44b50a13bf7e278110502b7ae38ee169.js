570f93b2d033ad867027d803828ba1b81acda6d7
joshuabrudnak <joshuabrudnak@oakland.edu>
2020-09-15 12:06:03 -0400

    Updates

---
+++ precache-manifest.44b50a13bf7e278110502b7ae38ee169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c952568e1cfafb7648fe117a6ed0ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c50e33496541d09d80f6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2025e6a3f6e7eb56686f",
+    "url": "/static/js/2.ff807a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff807a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50e33496541d09d80f6",
+    "url": "/static/js/main.39108b38.chunk.js"
+  },
+  {
+    "revision": "cfd15f4595e8ab9040e7",
+    "url": "/static/js/runtime-main.21fd3ff4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
